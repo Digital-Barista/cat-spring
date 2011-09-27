a1a876872702aec9206f7a45d34173e773c55ce6
Nathan Nelson <falken224@gmail.com>
2011-09-27 21:57:51 +0000

    Added XStream aliases.

--- cat-web/src/main/java/com/digitalbarista/cat/data/Client.java
+++ cat-web/src/main/java/com/digitalbarista/cat/data/Client.java
@@ -9,6 +9,9 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+@XStreamAlias("Client")
 @Entity
 @Table(name="client")
 public class Client {
--- cat-web/src/test/java/com/digitalbarista/cat/controller/CatControllerTest.java
+++ cat-web/src/test/java/com/digitalbarista/cat/controller/CatControllerTest.java
@@ -19,7 +19,7 @@ import com.digitalbarista.cat.model.NavigationItem;
 
 @ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/cat-web-servlet.xml",
 								 "file:src/main/webapp/WEB-INF/navigation.xml",
-								 "classpath:test-dao.xml"})
+								 "file:src/test/resources/test-dao.xml"})
 public class CatControllerTest extends AbstractTestNGSpringContextTests {
 
 	@Test
