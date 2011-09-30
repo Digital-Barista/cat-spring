2b76fba50ca7daef7f35bc462c38710d3831986a
Nathan Nelson <falken224@gmail.com>
2011-09-30 23:18:44 +0000

    Added stackdump files to the ignore list.

---
+++ cat-web/src/main/webapp/WEB-INF/db-scripts/008_create_simple_api_user.sql
@@ -0,0 +1,2 @@
+insert into users(username,password,enabled) values ('api-user','571a7ce8369dba839914dfb46ea6cb0aee97a4e0',1);
+insert into authorities(username,authority) values ('api-user','ROLE_API_USER');
