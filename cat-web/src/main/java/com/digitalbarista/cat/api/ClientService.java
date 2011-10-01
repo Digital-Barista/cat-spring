b50875ea8ba17ffb0261b0eb9d50543442222d16
Nathan Nelson <falken224@gmail.com>
2011-10-01 04:15:59 +0000

    Fixing Client Service mappings. Adding Client Service get by ID.

--- cat-web/src/main/java/com/digitalbarista/cat/api/ClientService.java
+++ cat-web/src/main/java/com/digitalbarista/cat/api/ClientService.java
@@ -3,8 +3,9 @@ package com.digitalbarista.cat.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,20 +13,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.digitalbarista.cat.dao.ClientDao;
 import com.digitalbarista.cat.data.Client;
 
-@Service("/clients*")  //The * is necessary for extension type mapping.
+@Controller
 @Transactional
 public class ClientService {
 	
 	@Autowired
 	private ClientDao clientDao;
 	
-	@RequestMapping(method={RequestMethod.GET})
+	@RequestMapping(value="/clients",method={RequestMethod.GET})
 	public List<Client> getAllClients()
 	{
 		return clientDao.getAllClients();
 	}
 	
-	@RequestMapping(method={RequestMethod.POST})
+	@RequestMapping(value="/clients/{id}",method={RequestMethod.GET})
+	public Client getClient(@PathVariable long id)
+	{
+		return clientDao.getClient(id);
+	}
+	
+	@RequestMapping(value="/clients",method={RequestMethod.POST})
 	public Client saveClient(@RequestBody Client client)
 	{
 		return clientDao.save(client);
