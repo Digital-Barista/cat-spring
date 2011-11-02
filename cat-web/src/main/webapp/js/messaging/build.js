1bf8e9866961fbef889480b29ff762fd6354582f
Kris Hoyt <shirtpoker@gmail.com>
2011-11-02 03:02:43 +0000

    Mocked some campaign builder

--- cat-web/src/main/webapp/js/messaging/build.js
+++ cat-web/src/main/webapp/js/messaging/build.js
@@ -12,15 +12,20 @@ dbi.Build = Class.extend({
   
   buildCampaign: function(){
     var fsa = Joint.dia.fsa;
-    var entry = fsa.State.create({ position: {x: 300, y: 50}, label: "Entry" });
+    var entry = fsa.State.create({ position: {x: 150, y: 50}, label: "Entry" });
     var message = fsa.State.create({ position: {x: 50, y: 150}, label: "Message" });
-    var coupon = fsa.State.create({ position: {x: 600, y: 150}, label: "Coupon" });
+    var coupon = fsa.State.create({ position: {x: 300, y: 150}, label: "Coupon" });
+    var message2 = fsa.State.create({ position: {x: 150, y: 250}, label: "Message" });
+    var coupon2 = fsa.State.create({ position: {x: 450, y: 250}, label: "Coupon" });
     
-    var all = [entry, message, coupon];
+    var all = [entry, message, coupon, message2, coupon2];
     
     var mArrow = Joint.dia.fsa.arrow;
-    entry.joint(message, (mArrow.label = 'Immediate', mArrow)).registerForever(all);
-    entry.joint(coupon, fsa.arrow).registerForever(all);
+    mArrow.label = 'Immediate';
+    entry.joint(message, mArrow).registerForever(all);
+    entry.joint(coupon, mArrow).registerForever(all);
+    coupon.joint(message2, mArrow).registerForever(all);
+    coupon.joint(coupon2, mArrow).registerForever(all);
   }
 });
 dbi.build = new dbi.Build();
